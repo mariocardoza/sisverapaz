5f1c2ae0619e2e958b46fd9f991afa0ebf29687b
Ingrid Ayala <ingridayala_94@hotmail.com>
2018-11-26 11:45:06 -0600

    Controller banco

---
+++ app/Http/Controllers/BancoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Banco;
+
+class BancoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $estado=(!isset($request->estado))?1:$request->estado;
+        $bancos=Banco::where('estado',$estado)->orderby('nombre')->get();
+        return view('bancos.index',compact('bancos','estado'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('bancos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $banco= new Banco();
+        $banco->nombre=$request->nombre;
+        $banco->save();
+
+        return redirect('bancos')->with('mensaje','Banco registrado');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $banco=Banco::find($id);
+        return view('bancos.edit',compact('banco'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $banco=Banco::find($id);
+        $banco->nombre=$request->nombre;
+        $banco->save();
+
+        return redirect('bancos')->with('mensaje','Registro modificado con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
