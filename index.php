a27cc1a07beb5a060fb28f503e5a599179b13889
Ingrid Ayala <ingridayala_94@hotmail.com>
2019-06-24 14:28:02 -0600

    Corrección index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="refresh" content="0;URL=/blissey/public">
+<meta http-equiv="refresh" content="0;URL=/sisverapaz/public">
 </head>
 <body>
 </body>
