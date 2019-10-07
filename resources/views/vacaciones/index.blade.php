d057f3fde1305fee1d2d3741c56467ca254d618a
Ingrid Ayala <ingridayala_94@hotmail.com>
2019-10-06 19:11:12 -0600

    Cambios en vacaciones

--- resources/views/vacaciones/index.blade.php
+++ resources/views/vacaciones/index.blade.php
@@ -3,11 +3,15 @@
 @section('migasdepan')
 <h1>
         Vacaciones
+        @if($estado==0)
         <small>Asignación de vacaciones</small>
+        @else
+        <small>Asignadas</small>
+        @endif
       </h1>
       <ol class="breadcrumb">
         <li><a href="{{ url('/vacaciones') }}"><i class="fa fa-dashboard"></i> Vacaciones</a></li>
-        <li class="active">Listado de empleados para asignar vacación</li>
+        <li class="active">Listado de empleados con vacaciones asignadas</li>
       </ol>
 @endsection
 
@@ -17,9 +21,11 @@
           <div class="box">
             <div class="box-header">
               <h3 class="box-title">Listado</h3>
-                {{-- <a href="{{ url('/empleados/create') }}" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Agregar</a>
-                <a href="{{ url('/empleados?estado=1') }}" class="btn btn-primary">Activos</a>
-                <a href="{{ url('/empleados?estado=2') }}" class="btn btn-primary">Papelera</a> --}}
+              @if($estado==0)
+                <a href="{{ url('/vacaciones?estado=2') }}" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span> Asignadas</a>
+              @else
+              <a href="{{ url('/vacaciones?estado=0') }}" class="btn btn-success"><span class="glyphicon glyphicon-plus-sign"></span>Por Asignar</a>
+              @endif
             </div>
             <!-- /.box-header -->
             
@@ -29,7 +35,13 @@
                   <th>N°</th>
                   <th>Nombre empleado</th>
                   <th>Inicio de labores</th>
-                  <th>Acción</th>
+                  @if($estado==0)
+                    <th>Acción</th>
+                  @else
+                  <th>Fecha del pago</th>
+                  <th>Pago</th>
+                  @endif
+                  
                 </thead>
                 <tbody>
                   @foreach($vacaciones as $index => $vacacion)
@@ -37,10 +49,15 @@
                     <td>{{ $index+1 }}</td>
                     <td>{{ $vacacion->detalleplanilla->empleado->nombre }}</td>
                     <td>{{ $vacacion->detalleplanilla->fecha_inicio->format('d-m-Y') }}</td>
-                    @php
-                        $pago=App\detalleplanilla::pago($vacacion->detalleplanilla->id);
-                    @endphp
-                    <td><button type="button" data-id="{{$vacacion->id}}" data-pago="{{$pago}}" class="btn btn-primary" name="button" id="btn_vacacion"><span class="glyphicon glyphicon-ok"></span></button></td>
+                    @if($estado==0)
+                      @php
+                          $pago=App\detalleplanilla::pago($vacacion->detalleplanilla->id);
+                      @endphp
+                      <td><button type="button" data-id="{{$vacacion->id}}" data-pago="{{number_format($pago,2)}}" class="btn btn-primary" name="button" id="btn_vacacion"><span class="glyphicon glyphicon-ok"></span></button></td>
+                    @else
+                    <td>{{ $vacacion->fecha_pago }}</td>
+                    <td>{{ number_format($vacacion->pago,2) }}</td>
+                    @endif
                   </tr>
                   @endforeach
                 </tbody>
