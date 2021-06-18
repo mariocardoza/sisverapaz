746b1951236819f610897d8ef60016c41dc36396
Mario Cardoza <mariokr.rocker@gmail.com>
2021-06-17 22:25:38 -0600

    fixed

--- database/seeds/RentaTableSeeder.php
+++ database/seeds/RentaTableSeeder.php
@@ -89,12 +89,12 @@ class RentaTableSeeder extends Seeder
      }
      public function truncateTables(array $tables)
      {
-         DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
 
          foreach ($tables as $table) {
              DB::table($table)->truncate();
          }
 
-         DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
      }
 }
